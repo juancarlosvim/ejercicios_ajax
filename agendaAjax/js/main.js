c2525316b7c0a769418e409c2c35fd9cd7da42a4
Juan Carlos <juancarlosvillegasmarmol@gmail.com>
2018-01-31 20:04:18 +0100

    Arreglar que cuando no esté un registro rellene los campos con undefined, también falta por probar el insertar

--- agendaAjax/js/main.js
+++ agendaAjax/js/main.js
@@ -1,38 +1,51 @@
 var debug = true;
 function iniciar() {
-    var btnInsertar = document.getElementById('btnInsertar');
+    "use strict";
+    /*var btnInsertar = document.getElementById('btnInsertar');
+    btnInsertar.addEventListener("click", insertarBD);*/
     var btnConsultar = document.getElementById('btnConsultar');
     btnConsultar.addEventListener("click", consultarBD);
-    var btnModificar = document.getElementById('btnModificar');
-    var btnEliminar = document.getElementById('btnELiminar');
+    /*var btnModificar = document.getElementById('btnModificar');
+    var btnEliminar = document.getElementById('btnELiminar');*/
 }
 
 function consultarBD() {
     var url = "http://daw2.iesoretania.es/recursos/bd_leer_agenda.php";
 
     var lecturaBD = function (e) {
-        var datos = JSON.parse(e.target.responseText);
+        var datos = e.target;
+        var estado = document.getElementById('estadoConexion');
+        var mensaje = document.getElementById('pMensaje');
         if(debug){
             console.log(datos);
         }
-        if(datos.status ===200){
-            if(debug){
-                console.log("Conexión correcta");
-            }
+        if (datos.status !== 200){
+            estado.textContent = "Error en la conexión";
+        }
+        else{
             var respuesta = JSON.parse(datos.responseText);
-            if(debug){
-                console.log(respuesta.row.length)
+            estado.textContent = "Conexión Correcta";
+            if (typeof respuesta.row !== "undefined"){
+                if (debug){
+                    console.log(respuesta.row.length);
+                }
+                if (respuesta.row.length === 0) {
+                    mensaje.textContent = "No encontrado";
+                }
+                else {
+                    mensaje.textContent = "Encontrado";
+                    document.getElementById("txtNombre").value = respuesta.row.Nombre;
+                    document.getElementById("txtApellidos").value = respuesta.row.Apellidos;
+                    document.getElementById("txtAlias").value = respuesta.row.Alias;
+                    document.getElementById("txtDireccion").value = respuesta.row.Direccion;
+                    document.getElementById("txtPoblacion").value = respuesta.row.Poblacion;
+                    document.getElementById("txtTelefono").value = respuesta.row.Telefono;
+                    document.getElementById("imgFoto").src = "data:image/jpg;base64, " + respuesta.row.foto;
+                }
+
+            }else{
+                mensaje.textContent = "No existe";
             }
-
-            document.getElementById("txtNombre").value = respuesta.row.Nombre;
-            document.getElementById("txtApellidos").value = respuesta.row.Apellidos;
-            document.getElementById("txtAlias").value = respuesta.row.Alias;
-            document.getElementById("txtDireccion").value = respuesta.row.Direccion;
-            document.getElementById("txtPoblacion").value = respuesta.row.Poblacion;
-            document.getElementById("txtTelefono").value = respuesta.row.Telefono;
-            document.getElementById("imgFoto").src="data:image/jpg;base64, "+respuesta.row.foto;
-
-
         }
     };
     var misDatos = (function () {
@@ -54,49 +67,52 @@ function insertarBD() {
     var url = "http://daw2.iesoretania.es/recursos/bd_insertar_agenda.php";
 
     var meterDatosBD = function (e) {
-        var datos = JSON.parse(e.target.responseText);
+        var datos = e.target;
+        var estado = document.getElementById('estadoConexion');
+        var mensaje = document.getElementById('pMensaje');
         if(debug){
             console.log(datos);
         }
-        if(datos.status ===200){
-            if(debug){
-                console.log("Conexión correcta");
-            }
+        if (datos.status !== 200) {
+            estado.textContent = "Error en la conexión";
+        }
+        else {
+
             var respuesta = JSON.parse(datos.responseText);
-            if(debug){
-                console.log(respuesta.row.length)
-            }
-
-
-
-
+            estado.textContent = "Conexión Correcta";
+           if(respuesta.row > 0){
+               mensaje.textContent = "Usuario registrado";
+               document.getElementById('txtId').value = respuesta.row1['LAST_INSERT_ID'];
+           }else{
+               mensaje.textContent = "Usuario no registrado, revise los campos";
+           }
         }
     };
-    var misDatos = (function () {
+    var myDate = (function () {
         var auxId = document.getElementById('txtId').value;
-        var nombre = document.getElementById("txtNombre").value;
-        var apellidos = document.getElementById("txtApellidos").value;
+        var name = document.getElementById("txtNombre").value;
+        var subName = document.getElementById("txtApellidos").value;
         var alias =  document.getElementById("txtAlias").value;
-        var direccion =  document.getElementById("txtDireccion").value;
-        var poblacion =   document.getElementById("txtPoblacion").value;
-        var telefono = document.getElementById("txtTelefono").value;
-        var foto =  document.getElementById("imgFoto").src="data:image/jpg;base64, ";
+        var direction =  document.getElementById("txtDireccion").value;
+        var population =   document.getElementById("txtPoblacion").value;
+        var phone = document.getElementById("txtTelefono").value;
+        var image =  document.getElementById("fFoto").files[0];
         var auxForm = new FormData();
         auxForm.append("txtid", auxId);
         auxForm.append("txtalias", alias);
-        auxForm.append("txtnombre", nombre);
-        auxForm.append("txtnombre", nombre);
-        auxForm.append("txtapellidos", apellidos);
-        auxForm.append("txtdireccion", direccion);
-        auxForm.append("txtpoblacion", poblacion);
-        auxForm.append("txttelefono", telefono);
-        auxForm.append("id", foto);
+        auxForm.append("txtnombre", name);
+        auxForm.append("txtnombre", name);
+        auxForm.append("txtapellidos", subName);
+        auxForm.append("txtdireccion", direction);
+        auxForm.append("txtpoblacion", population);
+        auxForm.append("txttelefono", phone);
+        auxForm.append("idFoto", image);
         return auxForm;
     })();
     var solicitud = new window.XMLHttpRequest();
     solicitud.addEventListener("load", meterDatosBD);
     solicitud.open("POST", url, true);
-    solicitud.send(misDatos);
+    solicitud.send(myDate);
 }
 
 /* MODIFICAR */
