5fa1f990dc4b50f31424fc8721d4dfb7a03a20d1
Juan Carlos <juancarlosvillegasmarmol@gmail.com>
2018-01-31 12:50:44 +0100

    falta modificar, insertar y eliminar

--- agendaAjax/js/main.js
+++ agendaAjax/js/main.js
@@ -46,6 +46,98 @@ function consultarBD() {
     solicitud.open("POST", url, true);
     solicitud.send(misDatos);
 }
+/*
+ INSERTAR
+ */
 
+function insertarBD() {
+    var url = "http://daw2.iesoretania.es/recursos/bd_insertar_agenda.php";
+
+    var meterDatosBD = function (e) {
+        var datos = JSON.parse(e.target.responseText);
+        if(debug){
+            console.log(datos);
+        }
+        if(datos.status ===200){
+            if(debug){
+                console.log("Conexión correcta");
+            }
+            var respuesta = JSON.parse(datos.responseText);
+            if(debug){
+                console.log(respuesta.row.length)
+            }
+
+
+
+
+        }
+    };
+    var misDatos = (function () {
+        var auxId = document.getElementById('txtId').value;
+        var nombre = document.getElementById("txtNombre").value;
+        var apellidos = document.getElementById("txtApellidos").value;
+        var alias =  document.getElementById("txtAlias").value;
+        var direccion =  document.getElementById("txtDireccion").value;
+        var poblacion =   document.getElementById("txtPoblacion").value;
+        var telefono = document.getElementById("txtTelefono").value;
+        var foto =  document.getElementById("imgFoto").src="data:image/jpg;base64, ";
+        var auxForm = new FormData();
+        auxForm.append("txtid", auxId);
+        auxForm.append("txtalias", alias);
+        auxForm.append("txtnombre", nombre);
+        auxForm.append("txtnombre", nombre);
+        auxForm.append("txtapellidos", apellidos);
+        auxForm.append("txtdireccion", direccion);
+        auxForm.append("txtpoblacion", poblacion);
+        auxForm.append("txttelefono", telefono);
+        auxForm.append("id", foto);
+        return auxForm;
+    })();
+    var solicitud = new window.XMLHttpRequest();
+    solicitud.addEventListener("load", meterDatosBD);
+    solicitud.open("POST", url, true);
+    solicitud.send(misDatos);
+}
+
+/* MODIFICAR */
+function modificarBD() {
+    var url = "http://daw2.iesoretania.es/recursos/bd_modificar_agenda.php";
+
+    var editarDatosBD = function (e) {
+        var datos = JSON.parse(e.target.responseText);
+        if(debug){
+            console.log(datos);
+        }
+        if(datos.status ===200){
+            if(debug){
+                console.log("Conexión correcta");
+            }
+            var respuesta = JSON.parse(datos.responseText);
+            if(debug){
+                console.log(respuesta.row.length)
+            }
+
+            document.getElementById("txtNombre").value = respuesta.row.Nombre;
+            document.getElementById("txtApellidos").value = respuesta.row.Apellidos;
+            document.getElementById("txtAlias").value = respuesta.row.Alias;
+            document.getElementById("txtDireccion").value = respuesta.row.Direccion;
+            document.getElementById("txtPoblacion").value = respuesta.row.Poblacion;
+            document.getElementById("txtTelefono").value = respuesta.row.Telefono;
+            document.getElementById("imgFoto").src="data:image/jpg;base64, "+respuesta.row.foto;
+
+
+        }
+    };
+    var misDatos = (function () {
+        var auxId = document.getElementById('txtId').value;
+        var auxForm = new FormData();
+        auxForm.append("txtid", auxId);
+        return auxForm;
+    })();
+    var solicitud = new window.XMLHttpRequest();
+    solicitud.addEventListener("load", modificarBD);
+    solicitud.open("POST", url, true);
+    solicitud.send(misDatos);
+}
 
 window.addEventListener("DOMContentLoaded", iniciar);
