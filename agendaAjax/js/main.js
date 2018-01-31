9de29777240ad5eda4e9b85d00246bd44f4a8ef8
Juan Carlos <juancarlosvillegasmarmol@gmail.com>
2018-01-31 12:18:53 +0100

    Consultar funciona

--- agendaAjax/js/main.js
+++ agendaAjax/js/main.js
@@ -11,23 +11,32 @@ function consultarBD() {
     var url = "http://daw2.iesoretania.es/recursos/bd_leer_agenda.php";
 
     var lecturaBD = function (e) {
-        var datos = e.target;
+        var datos = JSON.parse(e.target.responseText);
         if(debug){
             console.log(datos);
         }
         if(datos.status ===200){
             if(debug){
                 console.log("Conexión correcta");
-            }else{
-                console.log("Conexión incorrecta");
             }
             var respuesta = JSON.parse(datos.responseText);
+            if(debug){
+                console.log(respuesta.row.length)
+            }
+
+            document.getElementById("txtNombre").value = respuesta.row.Nombre;
+            document.getElementById("txtApellidos").value = respuesta.row.Apellidos;
+            document.getElementById("txtAlias").value = respuesta.row.Alias;
+            document.getElementById("txtDireccion").value = respuesta.row.Direccion;
+            document.getElementById("txtPoblacion").value = respuesta.row.Poblacion;
+            document.getElementById("txtTelefono").value = respuesta.row.Telefono;
+            document.getElementById("imgFoto").src="data:image/jpg;base64, "+respuesta.row.foto;
 
 
         }
     };
     var misDatos = (function () {
-        var auxId = document.getElementById('txtId');
+        var auxId = document.getElementById('txtId').value;
         var auxForm = new FormData();
         auxForm.append("txtid", auxId);
         return auxForm;
